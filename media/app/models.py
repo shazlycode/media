9682e410b0f9a6634713a340181176553c367ef1
shazlycode <dr.ahmedshazly@yahoo.com>
2020-02-22 00:54:50 +0000

    pillow

--- media/app/models.py
+++ media/app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class TV (models.Model):
-    tv_name=models.C
+class TV(models.Model):
+    tv_name= models.CharField(max_length=100)
+    tv_cat= models.CharField(max_length=100)
+    tv_link=models.CharField(max_length=300)
+    tv_img=models.ImageField(default='logo.jpg', upload_to='tvlogo')
