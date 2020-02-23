5a6689bad6487b0dc1a04c3b05658fbaeb79d0f1
shazlycode <dr.ahmedshazly@yahoo.com>
2020-02-23 01:57:07 +0000

    asddf

--- media/app/views.py
+++ media/app/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from app.models import TV
 # Create your views here.
 def index(request):
-    tvs=TV.objects.all()
+    tvsdrama=TV.objects.filter(tv_cat="drama")
+    tvssport=TV.objects.filter(tv_cat="sport")
     context={
         "title":"HOME",
-        'tvs':tvs,
+        'tvsdrama':tvsdrama,
+        'tvssport':tvssport,
     }
     return render(request, 'app/index.html', context)
